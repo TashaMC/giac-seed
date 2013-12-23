0d9e7593a81da93a2725c1cd8957bb92d3d9b7a1
sergiator <serj.kondrashov@gmail.com>
2013-12-23 16:22:31 +0200

    Template for grunt-ionic-angular-cordova project

---
+++ giac/.temp/app.concat.js
@@ -0,0 +1,12 @@
+angular.module('todo', ['ionic'])
+
+// You code goes here... 
+// Example:
+.controller('TodoCtrl', function($scope) {
+  $scope.tasks = [
+    { title: 'Collect coins' },
+    { title: 'Eat mushrooms' },
+    { title: 'Get high enough to grab the flag' },
+    { title: 'Find the Princess' }
+  ];
+});
